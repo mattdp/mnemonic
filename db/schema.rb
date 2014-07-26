0a56aabca64c84d8ca7a4573065f195080c1b08a
mattdp <matthew.frank.dupont@gmail.com>
2014-07-26 14:55:03 -0700

    add timestamps to people

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140726214901) do
+ActiveRecord::Schema.define(version: 20140726215159) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "people", force: true do |t|
-    t.date   "birthday"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
+    t.date     "birthday"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
