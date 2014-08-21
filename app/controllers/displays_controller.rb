040bbd96315937add8c937aad92ae8bc4c184fee
mattdp <matthew.frank.dupont@gmail.com>
2014-08-20 18:34:37 -0700

    show recent taggings on homepage

--- app/controllers/displays_controller.rb
+++ app/controllers/displays_controller.rb
@@ -2,5 +2,6 @@ class DisplaysController < ActionController::Base
   def index
     @birthdays = Event.get_displayable("birthday")
     @wants_and_has = Event.get_displayable("wants").concat(Event.get_displayable("has"))
+    @recent_taggings = Tagging.last(5).reverse
   end
 end
