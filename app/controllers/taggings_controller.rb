e6cb9d262058bbae81466e9cd58967f048416929
mattdp <matthew.frank.dupont@gmail.com>
2014-09-27 11:59:47 -0700

    taggings setup page

--- app/controllers/taggings_controller.rb
+++ app/controllers/taggings_controller.rb
@@ -1,6 +1,20 @@
 class TaggingsController < ActionController::Base
 
-  def new
+  def new #first new page, choose a verb and a tag
+    @verbs = Verb.all
+    @tags = Tag.all
+  end
+
+  def new_submission #route submit of new to appropriate intermediate_new page
+    verb_id = params["Verb"]
+    tag_id = params["Tag"]
+    redirect_to taggings_intermediate_new_path({verb_id: verb_id,tag_id: tag_id})
+  end
+
+  def intermediate_new #second new page, choose person(s) for verb and tag
+  end
+
+  def create #upon submission of intermediate_new
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Mnemonic::Application.routes.draw do
   get '/people/:id', to: 'people#edit'
   get '/people/generate_names', to: 'people#generate_names', as: 'people_generate_names'
 
-  resources :taggings, only: [:new]
-  get '/taggings/live_search', to: 'taggings#live_search'
+  resources :taggings, only: [:new, :create]
+  match '/taggings/new_submission', to: 'taggings#new_submission', via: :post, as: 'taggings_new_submission'
+  get '/taggings/intermediate_new/:verb_id/:tag_id', to: 'taggings#intermediate_new', as: "taggings_intermediate_new"
 
 end
