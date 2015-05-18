6f6a1c90541f9a836f51cd9b57f1d40124d9171b
mattdp <matthew.frank.dupont@gmail.com>
2015-05-17 20:41:27 -0400

    beginning devise installation - all command prompt suggestions implemented

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,9 @@
 Mnemonic::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #needed for devise
+  config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
