4f146a94b25c18a4b1a6f3c11ef3f8a63e9fa245
mattdp <matthew.frank.dupont@gmail.com>
2015-06-01 21:26:00 -0700

    navbar, remainder days display on people table

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Mnemonic::Application.routes.draw do
   get '/events/dismiss/:id/:dismissed_reason', to: 'events#dismiss', as: 'events_dismiss'
   get '/events/snooze/:id/:snooze_days', to: 'events#snooze', as: 'events_snooze'
 
-  get '/people/table', to: 'people#table'
+  get '/people/table', to: 'people#table', as: "people_table"
   get '/people/generate_names', to: 'people#generate_names', as: 'people_generate_names'
   resources :people, only: [:edit, :update]
   get '/people/:id', to: 'people#edit'
