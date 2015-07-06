7f24ae3bd986b68d1b39a7d1a2f4d5f3f90dc1f2
mattdp <matthew.frank.dupont@gmail.com>
2015-07-05 21:06:03 -0700

    create a tag and event, link tag to event, event and tag have correct names

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -20,4 +20,37 @@ class EventsController < ApplicationController
     redirect_to root_path, notice: "Birthdays generated."
   end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new
+    @event.content = params[:content]
+    @event.save
+
+    nfl = Tag.proper_name_for_link(params[:content])
+
+    tag = Tag.find_by_name(params[:content])
+    tag = Tag.find_by_name_for_link(nfl) if tag.nil?
+    if tag.present?
+      tag.event_id = @event.id
+      tag.save
+    else
+      tag = Tag.create({name: params[:content], 
+        name_for_link: nfl,
+        event_id: @event.id
+        })
+    end
+
+    redirect_to edit_event_path(@event), notice: "Event created."
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+  end
+
 end
