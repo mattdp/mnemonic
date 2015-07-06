51e038453ab2d8238fb4e0e64573d5c8570b215c
mattdp <matthew.frank.dupont@gmail.com>
2015-07-05 20:24:13 -0700

    chosen gem and tagged event associations

--- app/models/communication.rb
+++ app/models/communication.rb
@@ -9,11 +9,13 @@
 #  contents   :text
 #  created_at :datetime
 #  updated_at :datetime
+#  event_id   :integer
 #
 
 class Communication < ActiveRecord::Base
 
   belongs_to :person
+  belongs_to :event
 
   #stub so can use for event setting
   def canonical_date
