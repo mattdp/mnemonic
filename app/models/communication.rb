0ba86820e8940da0e27423735e3cc1e21143deaf
mattdp <matthew.frank.dupont@gmail.com>
2015-07-25 00:39:04 -0700

    successful events cause comms

--- app/models/communication.rb
+++ app/models/communication.rb
@@ -22,11 +22,11 @@ class Communication < ActiveRecord::Base
     self.created_at
   end
 
-  def self.create_event_related_communication!(event,person_id)
+  def self.create_event_related_communication!(event,person_id,contents)
     communication = Communication.new(medium: "live", 
       event_id: event.id,
       person_id: person_id,
-      contents: "Saw at event '#{event.content}'")
+      contents: contents)
     communication.when = event.happening_date if event.happening_date.present?
     communication.save
   end
