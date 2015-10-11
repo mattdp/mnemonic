7b08935b4735a0d9376f459c05289b2029f7687b
mattdp <matthew.frank.dupont@gmail.com>
2015-10-11 11:39:25 -0700

    working, no when though

--- app/controllers/communications_controller.rb
+++ app/controllers/communications_controller.rb
@@ -5,6 +5,20 @@ class CommunicationsController < ApplicationController
   end
 
   def create
+
+    if params["new_people"].present?
+      params["new_people"].each do |index_number,information|
+        if (information["medium"].present? and information["contents"].present?)
+          id = information["name_info"].match(/\[(\d*)\]/)[1].to_i
+          Communication.create(person_id: id,
+            medium: information["medium"],
+            contents: information["contents"]
+          )
+        end
+      end
+    end
+
+    redirect_to new_communication_path, notice: "Save attempted."
   end
 
 end
