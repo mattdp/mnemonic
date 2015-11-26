3d4c81422054c24abd1f3f94df85b0c114f3be0e
mattdp <matthew.frank.dupont@gmail.com>
2015-11-25 17:51:16 -0800

    think data types figured out

--- app/models/email_processer.rb
+++ app/models/email_processer.rb
@@ -7,9 +7,11 @@ class EmailProcessor
     puts "*\n*\n*\nEMAIL RECEIVED*\n*\n*\n"
     puts "@email class: #{@email.class}"
     puts "@email to class: #{@email.to.class}"
-    puts "To: #{@email.to[:email]}"
-    puts "From: #{@email.from[:email]}"
     puts "Contents: #{@email.subject} ::: #{@email.body}"
+    puts "From: #{@email.from[:email]}"
+    #https://github.com/thoughtbot/griddler
+    #is an array of hashes, since possible multiple to's
+    puts "To: #{@email.to[0][:email]}" 
   end
 
 end
