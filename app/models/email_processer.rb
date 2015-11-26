aa28ca24d23c768e8d647c40788704886580d940
mattdp <matthew.frank.dupont@gmail.com>
2015-11-25 17:46:32 -0800

    testing email data types

--- app/models/email_processer.rb
+++ app/models/email_processer.rb
@@ -5,6 +5,8 @@ class EmailProcessor
 
   def process
     puts "*\n*\n*\nEMAIL RECEIVED*\n*\n*\n"
+    puts "@email class: #{@email.class}"
+    puts "@email to class: #{@email.to.class}"
     puts "To: #{@email.to[:email]}"
     puts "From: #{@email.from[:email]}"
     puts "Contents: #{@email.subject} ::: #{@email.body}"
