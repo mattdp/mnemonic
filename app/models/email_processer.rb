cff69ffbb27d4ea4f7ca4a4d427db08412009f5f
mattdp <matthew.frank.dupont@gmail.com>
2015-11-25 17:36:16 -0800

    email receiver a bit more interesting

--- app/models/email_processer.rb
+++ app/models/email_processer.rb
@@ -5,6 +5,9 @@ class EmailProcessor
 
   def process
     puts "*\n*\n*\nEMAIL RECEIVED*\n*\n*\n"
+    puts "To: #{@email.to[:email]}"
+    puts "From: #{@email.from[:email]}"
+    puts "Contents: #{@email.subject} ::: #{@email.body}"
   end
-  
+
 end
