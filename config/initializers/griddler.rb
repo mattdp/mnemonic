2a79f8c1434fff199980bf1de7586dc95473ce73
mattdp <matthew.frank.dupont@gmail.com>
2015-11-28 13:49:26 -0800

    move email processor to lib

--- config/initializers/griddler.rb
+++ config/initializers/griddler.rb
@@ -1,5 +1,7 @@
 #from https://robots.thoughtbot.com/griddler-is-better-than-ever
 
 Griddler.configure do |config|
+  config.processor_class = EmailProcessor
+  config.processor_method = :process 
   config.email_service = :mailgun
 end
