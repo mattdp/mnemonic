856af906aa7fa8ea5b3295900f55af5c6766b175
mattdp <matthew.frank.dupont@gmail.com>
2015-11-28 13:52:13 -0800

    RAdmin working again, unsure what did to email though

--- config/initializers/griddler.rb
+++ config/initializers/griddler.rb
@@ -1,7 +1,5 @@
 #from https://robots.thoughtbot.com/griddler-is-better-than-ever
 
 Griddler.configure do |config|
-  config.processor_class = EmailProcessor
-  config.processor_method = :process 
   config.email_service = :mailgun
 end
